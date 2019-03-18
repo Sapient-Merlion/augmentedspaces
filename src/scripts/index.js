a53144a7b69aa0cd0895da26dd0bae43e1a912ad
Lee Liak Wee <liak.lee@publicissapient.com>
2019-03-18 12:18:17 +0800

    update

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -6,22 +6,7 @@ import anime from 'animejs';
 import * as magnificpopup from 'magnific-popup';
 import CanvasAnimate from './components/canvasAnimate';
 
-// import '!!file-loader?name=images/[name].[ext]!images/gallery/photo1.jpg';
-// import '!!file-loader?name=images/[name].[ext]!images/gallery/photo2.jpg';
-// import '!!file-loader?name=images/[name].[ext]!images/gallery/photo3.jpg';
-// import '!!file-loader?name=images/[name].[ext]!images/gallery/photo4.jpg';
-// import '!!file-loader?name=images/[name].[ext]!images/gallery/photo5.jpg';
-// import '!!file-loader?name=images/[name].[ext]!images/gallery/photo6.jpg';
-// import '!!file-loader?name=images/[name].[ext]!images/gallery/photo7.jpg';
-// import '!!file-loader?name=images/[name].[ext]!images/gallery/photo8.jpg';
-// import '!!file-loader?name=images/[name].[ext]!images/gallery/photo9.jpg';
-// import '!!file-loader?name=images/[name].[ext]!images/gallery/photo10.jpg';
-// import '!!file-loader?name=images/[name].[ext]!images/gallery/photo11.jpg';
-// import '!!file-loader?name=images/[name].[ext]!images/gallery/photo12.jpg';
-// import '!!file-loader?name=images/[name].[ext]!images/gallery/photo13.jpg';
-// import '!!file-loader?name=images/[name].[ext]!images/gallery/photo14.jpg';
-// import '!!file-loader?name=images/[name].[ext]!images/gallery/photo15.jpg';
-// import '!!file-loader?name=images/[name].[ext]!images/gallery/photo16.jpg';
+
 function importAll(r) {
   return r.keys().map(r);
 }
@@ -34,7 +19,7 @@ import '../styles/style.scss';
 window.jQuery = $;
 window.$ = $;
 
-// CanvasAnimate();
+CanvasAnimate();
 
 function initTabs() {
   $('.tabs').each(function() {
